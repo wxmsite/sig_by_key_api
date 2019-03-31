82f63fb6fe2cbe95a86209c708f6e718e115216d
wxmsite <1902726344@qq.com>
2019-03-31 12:47:05 +0800

    initial

---
+++ include/steem/plugins/sig_by_key_api/sig_by_key_api_plugin.hpp
@@ -0,0 +1,42 @@
+// 这是插件类声明的头文件，该文件名必须与plugin.json中的plugin_project字段和该插件目录
+// 中CMakeLists.txt的add_library声明的库名相同，如果3者不相同的话，在编译时，插件模板
+// 生成的文件中会无法正确匹配到该头文件，从而编译错误。
+
+#pragma once
+#include <steem/plugins/json_rpc/json_rpc_plugin.hpp>
+#include <appbase/application.hpp>
+
+#define STEEM_sig_by_key_PLUGIN_NAME "sig_by_key"
+
+namespace steem
+{
+namespace plugins
+{
+namespace demo
+{
+class sig_by_key_plugin : public appbase::plugin<sig_by_key_plugin>
+{
+public:
+  sig_by_key_plugin(){};
+  virtual ~sig_by_key_plugin(){};
+
+  // 用以声明该插件依赖哪些插件
+  APPBASE_PLUGIN_REQUIRES((steem::plugins::json_rpc::json_rpc_plugin))
+  // 必须拥有的一个方法name，注册时用以唯一标识该插件
+  static const std::string &name()
+  {
+    static std::string name = STEEM_sig_by_key_PLUGIN_NAME;
+    return name;
+  }
+
+  virtual void set_program_options(appbase::options_description &cli, appbase::options_description &cfg) override{};
+
+  virtual void plugin_initialize(const appbase::variables_map &options) override;
+  virtual void plugin_startup() override{};
+  virtual void plugin_shutdown() override{};
+
+  std::shared_ptr<class sig_by_key> api;
+};
+} // namespace demo
+} // namespace plugins
+} // namespace steem
