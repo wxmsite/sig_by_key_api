43e028ef0b5362284f860ee423f4081f3f9f4743
wxmsite <bebetter159@gmail.com>
2019-03-31 15:26:36 +0800

    format and begin my own test

--- include/steem/plugins/sig_by_key_api/sig_by_key_api.hpp
+++ include/steem/plugins/sig_by_key_api/sig_by_key_api.hpp
@@ -22,17 +22,38 @@ struct UserSecretKey
   relicxx::G2 b3;
   relicxx::G2 b4;
   relicxx::G1 b5;
+} struct Sig
+{
+  relicxx::G2 c0;
+  relicxx::G1 c5;
+  relicxx::G2 c6;
+  relicxx::G1 e1;
+  relicxx::G2 e2;
+  relicxx::GT e3;
+  relicxx::ZR x;
+  relicxx::ZR y;
+  relicxx::ZR z;
+} struct MasterPublicKey
+{
+  unsigned int l;
+  relicxx::G1 hibeg1;
+  vector<relicxx::G2> hG2;
+  relicxx::GT n;
 }
-// get_sum方法的输入参数
+// get_sig方法的输入参数
 struct get_sig_args
 {
   relicxx::ZR m;
+  relicxx::G2 b0;
+  relicxx::G2 b3;
+  relicxx::G2 b4;
+  relicxx::G1 b5;
 };
 
-// get_sum方法的输出参数
+// get_sig方法的输出参数
 struct get_sig_return
 {
-  int64_t sum;
+  Sig sig;
 };
 
 class sig_by_key
@@ -41,7 +62,7 @@ public:
   sig_by_key();
   ~sig_by_key();
 
-  DECLARE_API((get_sum))
+  DECLARE_API((get_sig))
 
 private:
   std::unique_ptr<detail::sig_by_key_impl> my;
@@ -51,5 +72,5 @@ private:
 } // namespace steem
 
 // 将方法输入、输出参数进行反射
-FC_REFLECT(steem::plugins::demo::get_sig_args, (nums))
-FC_REFLECT(steem::plugins::demo::get_sig_return, (sum))
+FC_REFLECT(steem::plugins::demo::get_sig_args, (m)(b0)(b3)(b4)(b5))
+FC_REFLECT(steem::plugins::demo::get_sig_return, (sig))
--- sig_by_key_api.cpp
+++ sig_by_key_api.cpp
@@ -17,15 +17,74 @@ public:
   sig_by_key_impl() {}
   ~sig_by_key_impl() {}
 
-  // get_sum 就是我们提供的一个API方法，将输入的数组进行求和
-  get_sum_return get_sum(const get_sum_args &args) const
+  // 返回用户签名
+  get_sig_return get_sig(const get_sig_args &args) const
   {
-    get_sum_return final{0};
-    for (auto num : args.nums)
-    {
-      final.sum += num;
-    }
+    get_sig_return final{0};
+    UserSecretKey usk;
+    usk.b0 = args.b0;
+    usk.b3 = args.b3;
+    usk.b4 = args.b4;
+    usk.b5 = args.b5;
+    Sig sig;
+    MasterPublicKey mpk;
+    getMpk();
+    sign(args.m, usk, sig, mpk);
+    final.sig = sig;
     return final;
+  }
+
+private:
+  void HibeGS::setup(MasterPublicKey &mpk, G2 &msk) const
+  {
+    const unsigned int l = 4;
+    ZR alpha = group.randomZR();
+    mpk.g = group.randomG1();
+    mpk.g2 = group.randomG2();
+    mpk.hibeg1 = group.exp(mpk.g, alpha);
+    //we setup four level HIBE here,the first level is Group identity,the second level is user identity
+    //the third level is the signed message,the last level is a random identity
+    mpk.l = 4;
+    for (unsigned int i = 0; i <= l; i++)
+    {
+      ZR h = group.randomZR();
+      mpk.hG2.push_back(group.exp(mpk.g2, h));
+    }
+    mpk.n = group.randomGT();
+    msk = group.exp(mpk.g2, alpha);
+  }
+  void HibeGS::sign(const ZR &m, const UserSecretKey &usk, Sig &sig, const MasterPublicKey &mpk)
+  {
+    const ZR gUserID = group.hashListToZR(getUserID());
+    const ZR gGroupID = group.hashListToZR(getGroupID());
+    //G(UserID),G(r4),k are public
+    const ZR r3 = group.randomZR();
+    //r4 use to blind identity
+    const ZR r4 = group.randomZR();
+    //user to encrypt identity to the group manager
+    const ZR k = group.randomZR();
+    relicxx::G2 res = group.mul(mpk.hG2.at(0), group.exp(mpk.hG2.at(1), gGroupID));
+    res = group.mul(res, group.exp(mpk.hG2.at(2), gUserID));
+    res = group.mul(res, group.exp(mpk.hG2.at(3), m));
+    res = group.exp(group.mul(res, group.exp(mpk.hG2.at(4), r4)), r3);
+    sig.c0 = group.mul(usk.b0, group.exp(usk.b3, m));
+    sig.c0 = group.mul(group.mul(sig.c0, group.exp(usk.b4, r4)), res);
+    sig.c5 = group.mul(usk.b5, group.exp(mpk.g, r3));
+
+    sig.c6 = group.mul(group.exp(mpk.hG2.at(2), gUserID), group.exp(mpk.hG2.at(4), r4));
+    sig.e1 = group.exp(mpk.g, k);
+    sig.e2 = group.exp(group.mul(mpk.hG2.at(0), group.exp(mpk.hG2.at(1), gGroupID)), k);
+
+    sig.e3 = group.exp(group.pair(mpk.g2, mpk.hibeg1), k);
+    sig.e3 = group.mul(sig.e3, group.exp(mpk.n, gUserID));
+    sig.x = gUserID;
+    sig.y = r4;
+    sig.z = k;
+  }
+  void getMpk(MasterPublicKey &mpk)
+  {
+    relicxx::G2 msk;
+    setup(mpk, msk);
   }
 };
 } // namespace detail
@@ -45,7 +104,7 @@ void sig_by_key_plugin::plugin_initialize(const appbase::variables_map &options)
   api = std::make_shared<sig_by_key>();
 }
 
-DEFINE_LOCKLESS_APIS(sig_by_key, (get_sum))
+DEFINE_LOCKLESS_APIS(sig_by_key, (get_sig))
 } // namespace demo
 } // namespace plugins
 } // namespace steem
